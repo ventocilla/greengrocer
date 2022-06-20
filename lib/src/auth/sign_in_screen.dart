76c2d2d1388bd116a4f9174c38c9bbd92c9e553a
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2022-06-20 18:02:36 -0300

    added signInScreen.dart

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Container(),
+    );
+  }
+}

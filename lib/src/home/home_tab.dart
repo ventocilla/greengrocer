a8cff0b7b0072c98618361f6bfd13c0d5e3a4ffa
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2022-07-31 08:45:47 -0300

    added Badge and Search Field

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
@@ -31,16 +32,67 @@ class HomeTab extends StatelessWidget {
               ),
             ])),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: CustomColors.customSwatchColor,
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                badgeColor: CustomColors.customContrastColor,
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
             ),
           ),
         ],
       ),
-      // Campo de pesquisa
+
+      body: Column(
+        children: [
+          // Campo de pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui ...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
 
       // Categorias
 

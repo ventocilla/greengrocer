43962a9b7352c5c0ea0367a9d7da04ad2b6b46ac
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2022-08-08 11:31:43 -0300

    added shopping IconButton and price format

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -2,67 +2,97 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
 
+import '../../../services/utils_services.dart';
+
 class ItemTile extends StatelessWidget {
   final ItemModel item;
 
-  const ItemTile({
+  ItemTile({
     Key? key,
     required this.item,
   }) : super(key: key);
 
+  final UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Image
-            Expanded(
-              child: Image.asset(
-                item.imgUrl,
-              ),
-            ),
-
-            // Nome
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            // Preço - Unidade
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
-                  item.price.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: CustomColors.customSwatchColor,
+                // Image
+                Expanded(
+                  child: Image.asset(
+                    item.imgUrl,
                   ),
                 ),
+
+                // Nome
                 Text(
-                  '/${item.unit}',
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    fontSize: 12,
                   ),
+                ),
+
+                // Preço - Unidade
+                Row(
+                  children: [
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                  color: CustomColors.customSwatchColor,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    topRight: Radius.circular(20),
+                  )),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/src/services/utils_services.dart
@@ -0,0 +1,12 @@
+//import 'package:greengrocer/src/services/intl';
+
+import 'package:intl/intl.dart';
+
+class UtilsServices {
+  // R$ valor
+  String priceToCurrency(double price) {
+    NumberFormat numberFormat = NumberFormat.simpleCurrency(locale: 'pt_BR');
+
+    return numberFormat.format(price);
+  }
+}

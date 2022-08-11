2ee35bfe3b1d58368c44aaabdd55bfa47ff43850
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2022-08-11 08:23:22 -0300

    added container fake data

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  final ItemModel item;
-  const ProductScreen({
+  ProductScreen({
     Key? key,
     required this.item,
   }) : super(key: key);
+
+  final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +22,79 @@ class ProductScreen extends StatelessWidget {
             child: Image.asset(item.imgUrl),
           ),
           Expanded(
-              child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(50),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.shade600,
-                  offset: const Offset(0, 2),
+            child: Container(
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(50),
                 ),
-              ],
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Nome - Quantidade
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+
+                  // Preço
+                  Text(
+                    utilsServices.priceToCurrency(item.price),
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColors.customSwatchColor,
+                    ),
+                  ),
+
+                  // Descrição
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          item.description,
+                          style: const TextStyle(
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Botão
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Botão ...'),
+                  ),
+                ],
+              ),
             ),
-          ))
+          ),
         ],
       ),
     );
